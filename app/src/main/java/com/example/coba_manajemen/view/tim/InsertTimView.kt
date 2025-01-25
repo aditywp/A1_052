04f319cf3101c990698c52fddad0954751d5361b
Aditya <adityapermono8@gmail.com>
2025-01-25 21:18:19 +0700

    membuat tampilan form untuk entry data Tim

---
+++ app/src/main/java/com/example/coba_manajemen/view/tim/InsertTimView.kt
@@ -0,0 +1,139 @@
+package com.example.coba_manajemen.view.tim
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.coba_manajemen.viewmodel.PenyediaViewModel
+import com.example.coba_manajemen.ui.navigation.DestinasiNavigasi
+import com.example.coba_manajemen.viewmodel.tim.InsertTimUiEvent
+import com.example.coba_manajemen.viewmodel.tim.InsertTimUiState
+import com.example.coba_manajemen.viewmodel.tim.InsertTimViewModel
+import com.example.serverdatabasep12.ui.widget.CostumeTopAppBar
+import kotlinx.coroutines.launch
+
+object DestinasiAddTimEntry: DestinasiNavigasi {
+    override val route = "entry_Tim"
+    override val titleRes = "Entry TIM"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryTimScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertTimViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiAddTimEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ){ innerPadding ->
+        EntryTimBody(
+            insertTimUiState = viewModel.uiTimState,
+            onTimValueChange = viewModel::updateInsertTimState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertTim()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryTimBody(
+    insertTimUiState: InsertTimUiState,
+    onTimValueChange: (InsertTimUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ){
+        FormInput(
+            insertTimUiEvent = insertTimUiState.insertTimUiEvent,
+            onValueChange = onTimValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertTimUiEvent: InsertTimUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertTimUiEvent) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        OutlinedTextField(
+            value = insertTimUiEvent.namaTim,
+            onValueChange = {onValueChange(insertTimUiEvent.copy(namaTim = it))},
+            label = { Text("Nama Tim") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertTimUiEvent.deskripsiTim,
+            onValueChange = {onValueChange(insertTimUiEvent.copy(deskripsiTim = it))},
+            label = { Text("Deskripsi") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(
+                text = "isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
