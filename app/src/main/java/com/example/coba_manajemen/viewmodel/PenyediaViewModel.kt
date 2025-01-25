aa14316fe531e8e4f5790b1199bf49413188c00b
Aditya <adityapermono8@gmail.com>
2025-01-25 22:31:53 +0700

    menambahkan PenyediaViewModel Factory untuk inisialisasi ViewModel CRUD Tugas

--- app/src/main/java/com/example/coba_manajemen/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/coba_manajemen/viewmodel/PenyediaViewModel.kt
@@ -1,55 +1,87 @@
-package com.example.coba_manajemen.viewmodel
+    package com.example.coba_manajemen.viewmodel
 
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.createSavedStateHandle
-import androidx.lifecycle.viewmodel.CreationExtras
-import androidx.lifecycle.viewmodel.initializer
-import androidx.lifecycle.viewmodel.viewModelFactory
-import com.example.coba_manajemen.ManajemenApplications
-import com.example.coba_manajemen.viewmodel.proyek.DetailProyekViewModel
-import com.example.coba_manajemen.viewmodel.proyek.HomeProyekViewModel
-import com.example.coba_manajemen.viewmodel.proyek.InsertProyekViewModel
-import com.example.coba_manajemen.viewmodel.proyek.UpdateProyekViewModel
-import com.example.coba_manajemen.viewmodel.tim.DetailTimViewModel
-import com.example.coba_manajemen.viewmodel.tim.HomeTimViewModel
-import com.example.coba_manajemen.viewmodel.tim.InsertTimViewModel
-import com.example.coba_manajemen.viewmodel.tim.UpdateTimViewModel
+    import androidx.lifecycle.ViewModelProvider
+    import androidx.lifecycle.createSavedStateHandle
+    import androidx.lifecycle.viewmodel.CreationExtras
+    import androidx.lifecycle.viewmodel.initializer
+    import androidx.lifecycle.viewmodel.viewModelFactory
+    import com.example.coba_manajemen.ManajemenApplications
+    import com.example.coba_manajemen.viewmodel.proyek.DetailProyekViewModel
+    import com.example.coba_manajemen.viewmodel.proyek.HomeProyekViewModel
+    import com.example.coba_manajemen.viewmodel.proyek.InsertProyekViewModel
+    import com.example.coba_manajemen.viewmodel.proyek.UpdateProyekViewModel
+    import com.example.coba_manajemen.viewmodel.tim.DetailTimViewModel
+    import com.example.coba_manajemen.viewmodel.tim.HomeTimViewModel
+    import com.example.coba_manajemen.viewmodel.tim.InsertTimViewModel
+    import com.example.coba_manajemen.viewmodel.tim.UpdateTimViewModel
+    import com.example.coba_manajemen.viewmodel.tugas.DetailTugasViewModel
+    import com.example.coba_manajemen.viewmodel.tugas.HomeTugasViewModel
+    import com.example.coba_manajemen.viewmodel.tugas.InsertTugasViewModel
+    import com.example.coba_manajemen.viewmodel.tugas.UpdateTugasViewModel
 
 
-object PenyediaViewModel {
-    val Factory = viewModelFactory {
-        //HOME PROYEK
-        initializer { HomeProyekViewModel(aplikasiManajemen().container.proyekRepository) }
-        initializer { InsertProyekViewModel(aplikasiManajemen().container.proyekRepository) }
-        initializer {
-            DetailProyekViewModel(
-                createSavedStateHandle(),
-                aplikasiManajemen().container.proyekRepository
-            )
-        }
-        initializer {
-            UpdateProyekViewModel(
-                createSavedStateHandle(),
-                aplikasiManajemen().container.proyekRepository
-            )
-        }
-        //HOME TIM
-        initializer { HomeTimViewModel(aplikasiManajemen().container.timRepository) }
-        initializer { InsertTimViewModel(aplikasiManajemen().container.timRepository) }
-        initializer {
-            DetailTimViewModel(
-                createSavedStateHandle(),
-                aplikasiManajemen().container.timRepository
-            )
-        }
-        initializer {
-            UpdateTimViewModel(
-                createSavedStateHandle(),
-                aplikasiManajemen().container.timRepository
-            )
+    object PenyediaViewModel {
+        val Factory = viewModelFactory {
+
+            //HOME PROYEK
+            initializer { HomeProyekViewModel(aplikasiManajemen().container.proyekRepository) }
+            initializer { InsertProyekViewModel(aplikasiManajemen().container.proyekRepository) }
+            initializer {
+                DetailProyekViewModel(
+                    createSavedStateHandle(),
+                    aplikasiManajemen().container.proyekRepository
+                )
+            }
+            initializer {
+                UpdateProyekViewModel(
+                    createSavedStateHandle(),
+                    aplikasiManajemen().container.proyekRepository
+                )
+            }
+
+            //HOME TIM
+            initializer { HomeTimViewModel(aplikasiManajemen().container.timRepository) }
+            initializer { InsertTimViewModel(aplikasiManajemen().container.timRepository) }
+            initializer {
+                DetailTimViewModel(
+                    createSavedStateHandle(),
+                    aplikasiManajemen().container.timRepository
+                )
+            }
+            initializer {
+                UpdateTimViewModel(
+                    createSavedStateHandle(),
+                    aplikasiManajemen().container.timRepository
+                )
+            }
+
+            //HOME TUGAS
+            initializer { HomeTugasViewModel(aplikasiManajemen().container.tugasRepository) }
+            initializer {
+                InsertTugasViewModel(
+                    aplikasiManajemen().container.tugasRepository,
+                    aplikasiManajemen().container.timRepository,
+                    aplikasiManajemen().container.proyekRepository
+                )
+            }
+            initializer {
+                DetailTugasViewModel(
+                    createSavedStateHandle(),
+                    aplikasiManajemen().container.tugasRepository,
+                    aplikasiManajemen().container.timRepository,
+                    aplikasiManajemen().container.proyekRepository
+                )
+            }
+            initializer {
+                UpdateTugasViewModel(
+                    createSavedStateHandle(),
+                    aplikasiManajemen().container.tugasRepository,
+                    aplikasiManajemen().container.timRepository,
+                    aplikasiManajemen().container.proyekRepository
+                )
+            }
         }
     }
-}
 
-fun CreationExtras.aplikasiManajemen(): ManajemenApplications =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ManajemenApplications)
+    fun CreationExtras.aplikasiManajemen(): ManajemenApplications =
+        (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ManajemenApplications)
