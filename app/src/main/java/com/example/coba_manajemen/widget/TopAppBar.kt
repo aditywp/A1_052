a50f236e12dab3a7e635af3cf8b89acced92d553
Aditya <adityapermono8@gmail.com>
2025-01-28 21:36:06 +0700

    memperbagus topappbar seperti mengubah warnanya

--- app/src/main/java/com/example/coba_manajemen/widget/TopAppBar.kt
+++ app/src/main/java/com/example/coba_manajemen/widget/TopAppBar.kt
@@ -1,6 +1,8 @@
 package com.example.serverdatabasep12.ui.widget
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -8,10 +10,18 @@ import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.coba_manajemen.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,13 +34,27 @@ fun CostumeTopAppBar(
     onRefresh: () -> Unit = {}
 ){
     CenterAlignedTopAppBar(
-        title = { Text(title)},
-        actions = {
-            Icon(
-                imageVector = Icons.Default.Refresh,
-                contentDescription = "",
-                modifier = Modifier.clickable { onRefresh() }
+        title = {  Text(
+            text = title,
+            style = MaterialTheme.typography.titleLarge.copy(
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontSize = 20.sp
             )
+        )},
+        actions = {
+            IconButton(
+                onClick = { onRefresh() },
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .padding(4.dp)
+            )
+            {
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = "",
+                    modifier = Modifier.clickable { onRefresh() }
+                )
+            }
         },
         modifier = modifier,
         scrollBehavior = scrollBehavior,
@@ -43,6 +67,12 @@ fun CostumeTopAppBar(
                     )
                 }
             }
-        }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = colorResource(id = R.color.green),
+            navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            actionIconContentColor = MaterialTheme.colorScheme.onPrimary,
+            titleContentColor = MaterialTheme.colorScheme.onPrimary
+        )
     )
 }
